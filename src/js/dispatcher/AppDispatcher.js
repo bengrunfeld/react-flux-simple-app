ceca585e7f09d78cd2cc5153e3dc44391958b334
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-03-02 20:59:13 -0800

    Fix AppDispatcher definition
    Previous code was throwing errors because it was from old versions of
    Flux. The new version requires that you use the `new` keyword with
    `Dispatcher`. Just check the diff to see what I mean.

--- src/js/dispatcher/AppDispatcher.js
+++ src/js/dispatcher/AppDispatcher.js
@@ -1,9 +1,8 @@
 var Dispatcher = require('flux').Dispatcher;
 var assign = require('object-assign');
 
-var AppDispatcher = assign({}, Dispatcher.prototype, {
+var AppDispatcher = assign(new Dispatcher(), {
   handleViewAction: function(action) {
-    console.log('action', action);
     this.dispatch({
       source: 'VIEW_ACTION',
       action: action
