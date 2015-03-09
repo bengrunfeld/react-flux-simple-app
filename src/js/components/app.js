cab18cd66bf88e7cd5b8bd75d69d8d7dc330a127
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-03-08 22:41:26 -0700

    Rename store file

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,7 @@
 /** @jsx React.DOM */
 var React = require('react');
 var AppActions = require('../actions/AppActions');
-var AppStore = require('../stores/AppStores');
+var AppStore = require('../stores/AppStore');
 
 var App = React.createClass({
     handleClick:function(){
