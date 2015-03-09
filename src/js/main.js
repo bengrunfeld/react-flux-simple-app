8293ce538d186dc0d272ce7e30b54891ecaafabd
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-03-08 22:26:13 -0700

    Remove link to twitter bootstrap css

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ var React = require('react');
 
 // Not ideal to use createFactory, but don't know how to use JSX to solve this
 // Posted question at: https://gist.github.com/sebmarkbage/ae327f2eda03bf165261
-var App = React.createFactory(require('./components/app.js'));
+var App = require('./components/app.js');
 
 React.render(
   <App />,
