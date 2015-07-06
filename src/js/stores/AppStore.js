15c091bc085d9c25958031eb17abc94ff73df610
Denis Savenok <denissavenok@gmail.com>
2015-07-06 18:01:57 +0400

    removed non-used AppConstants requirement

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -1,6 +1,5 @@
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var EventEmitter = require('events').EventEmitter;
-var AppConstants = require('../constants/AppConstants');
 var assign = require('object-assign');
 
 
